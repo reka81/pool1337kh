6778e12c927e17f0c8b49e1a84d18e39968d3edb
Ouadie El Fengour <ouadie.xyz1@gmail.com>
2023-08-14 01:05:48 +0100

    Update ft_interval.c
    Comment in the code explaining everything

--- exercices/arguments/ft_interval.c
+++ exercices/arguments/ft_interval.c
@@ -1,38 +1,43 @@
 #include <unistd.h>
 
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(str[i] != '\0')
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
+	while(str[i])
+		write(1, &str[i++], 1);
 }
-int main(int ac, char **av)
+
+// WE WILL USE TO INDEXES ONE TO COUNT THE STRING AS USUAL STARTING FROM 0
+// AND ONE TO COUNT THE LETTERS TO CHANGE THEM TO EITHER 3 OR 5 STARTING FROM 1
+// IN THE EXAMPLE WE HAVE "it's me mario hehe" RESULTS IN "it5s35e 53r5o 5eh5"
+// THE ' IS TREATED AS THE 3 CHAR SO WE HAVE TO TAKE THAT INTO ACCOUNT AND USE A SECOND INDEX
+// WE DON'T NEED A SEPERATE CASE FOR % 3 && % 5 SINCE IT IS INCLUDED IN THE % 3
+// BECAUSE NO MATTER WHAT, IF IT IS % 3 WE CHANGE THE CHAR TO 5 WHETER IT IS % 5 OR NOT
+int	main(int ac, char **av)
 {
-	int i;
+	int	i;
+	int	j:
 
 	i = 0;
+	j = 1;   // EXAMPLE START COUNTING WITH 1 NOT 0  
 	if(ac == 2)
 	{
 		while(av[1][i] != '\0')
 		{
-			if(i % 3 == 0 && i % 5 == 0)
+			if (i == 0) // SKIP FIRST CHAR CAUSE 0 % ANY NUMBER ALWAYS == 0
 			{
-				av[1][i] = '5'; 
+				i++;
+				j++;
 			}
-			else if(i % 3 == 0)
-			{
+			if(j % 3 == 0)
 				av[1][i] = '5';
 			}
-			else if(i % 5 == 0)
-			{
+			else if(j % 5 == 0)
 				av[1][i] = '3';
-			}
 			i++;
+			j++;
 		}
 		ft_putstr(av[1]);
 	}
