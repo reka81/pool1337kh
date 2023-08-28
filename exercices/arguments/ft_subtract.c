7e234cfa32eba5cb645ce15ee65ca0f4a5828d46
Ouadie El Fengour <ouadie.xyz1@gmail.com>
2023-08-28 16:09:00 +0100

    Update ft_subtract.c

--- exercices/arguments/ft_subtract.c
+++ exercices/arguments/ft_subtract.c
@@ -1,22 +1,18 @@
 //NOTICE:THE FUNCTION MAY VARY FROM ADD ,SUBTRACT,DIVIDE AND MULTIPLY LEARN TO BE FLEXIBLE AND DONT JUST MEMORIZE THE CODE (SAME GOES FOR A LOT OF FUNCTIONS U WILL FIND ON REPOSITORIES U FIND ONLINE)//
 void	ft_subtract(int a, int *ptr)
 {
-	*ptr = *ptr - a;
+	*ptr -= a;
 }
 //DONT PUSH THE INT MAIN FUNCTION ITS SOLE PURPOSE IS TESTING//
 #include <stdio.h>
 
-int main()
-{
-	int a;
-	int *ptr;
-	int c;
-	
-	a = 5;
-	c = 6;
-	ptr = &c;
-	
-	ft_subtract(a, ptr);
-	printf("%d", c);
-}
+int main() {
+    int num = 110;
+    int result = 60;
 
+    printf("Before: num = %d\n", num);
+    ft_subtract(result, &num);
+    printf("After: num = %d\n", num);
+
+    return 0;
+}
